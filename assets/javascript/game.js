b6b1ebd733f1525a5a9cf5849b28a10c2cfdce0b
WslyStvnChng <mr.cheng.st@gmail.com^C>
2018-01-06 15:07:57 -0700

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,90 @@
+//Making sure the DOM is ready, once the code is ready, it will manipulate the DOM to get it working
+$(document).ready(function() {
+console.log("Get ready to click");
+
+//create an array of strings for topics
+var topics = ['McLaren', 'Ferrari', 'Audi', 'Bugatti', 'Lamborghini', 'Pagani', 'Aston Martin'];
+
+
+
+function createButtons(){
+	$('.buttons').empty();
+	for (var i= 0; i < topics.length; i++){
+	$('.buttons').append("<button data-name='" + topics[i] + "'>" + topics[i] + "</button>");
+	}
+};
+
+
+createButtons();
+
+$(".buttons").on("click", "button", function() {
+	event.preventDefault();
+	var buttonText = $(this).attr("data-name");
+	var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + buttonText + "&limit=10&api_key=dc6zaTOxFJmzC";
+
+	$.ajax({
+		url: queryUrl,
+		method: 'GET'
+	}).done(function(response) {
+		console.log(response);
+		for (var i = 0; i < response.data.length; i++) {
+
+			// HEY GUYS! See below for the stuff of interest!
+			// The code below does the same thing that the messy code 
+			// block we had before does... but it's a lot cleaner
+			// and easier to make sense of. Message me if you have
+			// any questions on the what/why/how of it!
+
+			// first, each time we loop through, we're creating a new img tag
+			var newDiv = $("<div />").addClass("divContent");
+			var newImg = $("<img class='giphy' />");
+      var rating= response.data[i].rating;
+      var p = $('<h6>').text("Ratings: " + rating);
+
+			// then, with each chained '.attr', we add the different data
+			// attributes as well as the img src attribute
+			newImg.attr("data-state", "still")
+				
+				.attr("data-still", response.data[i].images.fixed_height_small_still.url)
+				.attr("data-anim", response.data[i].images.fixed_height_small.url)
+				.attr("src", response.data[i].images.fixed_height_small_still.url)
+			// then, the last thing we do is we append the newImg to the coolGifs container
+			
+			newDiv.append(newImg).append(p);
+			$(".coolGifs").append(newDiv);
+
+		}
+	});
+
+});
+
+$(".coolGifs").on("click", ".giphy", function() {
+	event.preventDefault();
+	// clear the coolGifs container first!!!
+	// $('.giphy').empty();
+
+	var gifState = $(this).attr("data-state");
+	var animUrl = $(this).attr("data-anim");
+	var stillUrl = $(this).attr("data-still");
+
+	if (gifState == "still") {
+		$(this).attr("src", animUrl).attr('data-state', 'animate');
+		 // this sets the src attribute to the animated url
+		// set 'data-state' to be equal to 'anim'
+
+	} else {
+		$(this).attr("src", stillUrl).attr('data-state', 'still');
+		// set src to be equal to value to data-still
+		// set 'data-state' to be equal to still
+	}
+
+});
+
+// Add a click event for the submit button in the 'add animal' section
+// This click event should call the 'createNewButton' function
+
+// this is calling it initially --- you'll be calling it again 
+// each time a new button is created
+createButtons();
+
+});
